be456c8428cba17dbc33ccf242660046eb78e680
ynit2000 <84170843+ynit2000@users.noreply.github.com>
2023-12-29 00:16:14 +0530

    Third

--- 03_conversion.js
+++ 03_conversion.js
@@ -29,3 +29,46 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(stringNumber)
 console.log(typeof stringNumber)
+
+
+
+// ********************** Operations *****************************
+console.log("*****************Operation******************")
+let value = 3
+let negValue = -value
+console.log(negValue)
+
+
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2**3)
+console.log(2/2)
+console.log(2%3)
+
+let str1 ="Hello! "
+let str2 = "Niteshah"
+
+let str3 = str1 + str2
+console.log(str3)
+
+
+console.log("1" +2)
+console.log(1 + "2")
+console.log('1' +2 +2)
+console.log(1+2 +'2')
+
+
+console.log(+true)
+console.log(+"")
+
+let num1,num2,num3
+
+num1 = num2 =num3 = 2+2
+let gameCounter = 100
+
+gameCounter++;
+console.log(gameCounter)
+
+
+
