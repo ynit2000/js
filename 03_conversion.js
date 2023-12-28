d53365badcbee03f7256b3d860e739488905a8f1
ynit2000 <84170843+ynit2000@users.noreply.github.com>
2023-12-28 23:49:25 +0530

    Third

---
+++ 03_conversion.js
@@ -0,0 +1,31 @@
+let score = "33"
+
+
+console.log(typeof score)
+console.log(typeof(score))
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+console.log(score)
+
+
+// "33" => 33
+//  "33abc" => NaN (not a number)
+// true => 1 ; false => 0
+
+let isLoggedIn = "hghv"
+
+let booleanInLogged = Boolean(isLoggedIn)
+console.log(booleanInLogged)
+
+// 1 => ture ; ;0 => fasle
+//  "" => false 
+// "niteshah" => ture
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
