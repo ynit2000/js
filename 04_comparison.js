b417e054a460b635a11452c373048aaf194fa57e
ynit2000 <84170843+ynit2000@users.noreply.github.com>
2023-12-29 10:57:51 +0530

    Fouth

---
+++ 04_comparison.js
@@ -0,0 +1,29 @@
+/*
+
+console.log(2>1)
+console.log(2>=1)
+console.log(2<1)
+console.log(2<=1)
+console.log(2 != 1)
+ // basic comparison   best 
+*/
+
+console.log("2" > 1)
+console.log("02" > 1)
+
+
+console.log(null > 0)
+console.log(null ==  0)
+console.log(null >= 0)
+ // what acturally js do is convert null into "NaN" ,'0' then compare
+
+ console.log(undefined == 0)
+ console.log(undefined > 0)
+ console.log(undefined < 0)
+// avoid this comparison    ******* Clean Code is Value ********
+
+
+ // === checking striklty
+
+ console.log('2'=== 2)
+
