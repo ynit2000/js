e55c925eb224ca020ee083bd5a1bc94cf2e66e30
ynit2000 <84170843+ynit2000@users.noreply.github.com>
2024-01-01 11:10:54 +0530

    iife

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,15 @@
+// Immediately Invoked Function Expression (IIFE)
+
+(function chai(){
+    // name IIFE
+    console.log(`DB CONNECTED`);
+
+})();
+// to avoid global pollution 
+
+// () --> defineation , ()---> excusion 
+
+( (name) => {
+    console.log(`DB CONNECTED Two ${name} `);
+}) ('ritesh')
+
