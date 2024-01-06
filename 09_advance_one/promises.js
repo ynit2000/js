4c614d34deb746aaf5ecce1635c389bd097a8f45
ynit2000 <84170843+ynit2000@users.noreply.github.com>
2024-01-07 00:09:12 +0530

    main

---
+++ 09_advance_one/promises.js
@@ -0,0 +1,98 @@
+const promiseOne = new Promise(function(resolve, reject){
+    //Do an async task
+    // DB calls, cryptography, network
+    setTimeout(function(){
+        console.log('Async task is compelete');
+        resolve()
+    }, 1000)
+})
+
+promiseOne.then(function(){
+    console.log("Promise consumed");
+})
+
+new Promise(function(resolve, reject){
+    setTimeout(function(){
+        console.log("Async task 2");
+        resolve()
+    }, 1000)
+
+}).then(function(){
+    console.log("Async 2 resolved");
+})
+
+const promiseThree = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        resolve({username: "Chai", email: "chai@example.com"})
+    }, 1000)
+})
+
+promiseThree.then(function(user){
+    console.log(user);
+})
+
+const promiseFour = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        let error = true
+        if (!error) {
+            resolve({username: "hitesh", password: "123"})
+        } else {
+            reject('ERROR: Something went wrong')
+        }
+    }, 1000)
+})
+
+ promiseFour
+ .then((user) => {
+    console.log(user);
+    return user.username
+}).then((username) => {
+    console.log(username);
+}).catch(function(error){
+    console.log(error);
+}).finally(() => console.log("The promise is either resolved or rejected"))
+
+
+
+const promiseFive = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        let error = true
+        if (!error) {
+            resolve({username: "js", password: "123"})
+        } else {
+            reject('ERROR: JS went wrong')
+        }
+    }, 1000)
+});
+
+
+async function consumePromiseFive(){
+    try{
+        const response = await promiseFive
+        console.log(response);
+} catch (error){
+    console.log(error);
+
+}}
+
+consumePromiseFive()
+
+// async function getAllusers(){
+//     try {
+//         const response = await fetch('https://jsonplacholdertypicode.com/users')
+//         const data = await response.json()
+//         console.log(data)
+        
+//     } catch (error) {
+//         console.log('E:', error);
+//     }
+// }
+
+// getAllusers()
+
+fetch('https://api.github.com/users/hiteshchoudhary')
+.then((response) => {
+    return response.json()
+}).then( () => {
+   console.log(data);
+}).catch((error) => console.log(error))
