8cd354bf94dca5113b408ec5c7a11b229dea1b50
ynit2000 <84170843+ynit2000@users.noreply.github.com>
2024-01-08 11:03:55 +0530

    DefineProp_Set_Get

---
+++ 10_classes_and_oop/properties_get_set.js
@@ -0,0 +1,27 @@
+// function based getter and setter 
+function User(email, password){
+    this._email = email;
+    this._password = password;
+
+    Object.defineProperty(this, "email",{
+        get: function(){
+            return this._email.toUpperCase()
+        },
+        set: function(value){
+            this._email = value;
+        }
+    })
+
+    Object.defineProperty(this, "password",{
+        get: function(){
+            return this._password.toUpperCase()
+        },
+        set: function(value){
+            this._password = value;
+        }
+    })
+}
+
+const private = new User("private@work.ai", "123")
+console.log(private.password);
+console.log(private.email);
